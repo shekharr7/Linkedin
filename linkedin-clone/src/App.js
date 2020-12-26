92e93f94b3496ab72cedf893dbfe60a3d129337c
Shekhar R <a-8177@LE1125.domain.name>
2020-12-26 13:38:09 +0530

    initial commit

---
+++ linkedin-clone/src/App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+        {/*Header*/}
+        <Header />
+        {/*App body*/}
+            {/*Sidebar*/}
+            {/*Feed*/}
+            {/*Widget*/}
+    </div>
+  );
+}
+
+export default App;
